d1f7e52af13cb395749be94242d63c5e2a805751
Douglas Alves <douglas.expalves@gmail.com>
2017-12-23 19:06:00 -0200

    update loggify

--- src/config/loggify.ts
+++ src/config/loggify.ts
@@ -1,19 +1,19 @@
-import { createWriteStream } from 'fs'
+import { createWriteStream, WriteStream } from 'fs'
 import { join } from 'path'
 import morgan from 'morgan'
 
-const sucessLog = createWriteStream(join(__dirname, '../../logs/sucess.log'), { flags: 'a' })
-const errorsLog = createWriteStream(join(__dirname, '../../logs/errors.log'), { flags: 'a' })
-const generalLog = createWriteStream(join(__dirname, '../../logs/general.log'), { flags: 'a' })
+const sucessLog : WriteStream = createWriteStream(join(__dirname, '../../logs/sucess.log'), { flags: 'a' })
+const errorsLog : WriteStream = createWriteStream(join(__dirname, '../../logs/errors.log'), { flags: 'a' })
+const generalLog : WriteStream = createWriteStream(join(__dirname, '../../logs/general.log'), { flags: 'a' })
 
-const logFormat = ':method :url :status :res[content-length] - :response-time ms'
+const logFormat : String = ':method :url :status :res[content-length] - :response-time ms'
 
 const sucessInfo = morgan(logFormat,{ 
   skip: (req, res) => { 
     return res.statusCode > 299 
   }, stream: sucessLog })
 
-const errorsInfo = morgan(`${logFormat} --`, { 
+const errorsInfo = morgan(logFormat, { 
   skip: (req, res) => { 
     return res.statusCode < 300 
   }, stream: errorsLog 
