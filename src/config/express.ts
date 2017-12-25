6f4c1ef866d4074429ea78e2ac3cc12dd2a0b976
Douglas Alves <douglas.expalves@gmail.com>
2017-12-24 23:23:02 -0200

    fixed express import and imported user controller in express

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,15 +1,17 @@
-import express from 'express'
+import express = require('express')
 import { urlencoded, json } from 'body-parser'
 
 import logs from './loggify'
 import middlewares from './middleware'
 import env from './env'
+import user from './../api/controllers/userController'
 
 const app = express()
 
 app.set('port', env.PORT || 3000)
 app.use([urlencoded({ extended: true }), json()])
 app.use([...logs, middlewares])
+app.use('/api', [user])
 
 app.listen(app.set('port'), () => {
   console.log(`BACKEND is runing on ${app.set('port')}`)
