6867b2f315d4b39fef23532c112577de8b8ec74c
Douglas Alves <douglas.expalves@gmail.com>
2017-12-30 10:43:44 -0200

    conn initial commit

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,4 +1,7 @@
-import Mongo from 'mongoose'
+import Mongo = require('mongoose')
+import bluebird = require('bluebird')
+
+bluebird.promisifyAll(Mongo)
 
 const Connection = Mongo.connect(process.env.mongo_uri || 'mongodb://localhost/typed', {
   useMongoClient: true
