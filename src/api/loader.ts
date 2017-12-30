5baa3d4960e32225c91bb37bb8c91d2c6d862c30
Douglas Alves <douglas.expalves@gmail.com>
2017-12-30 10:42:40 -0200

    controller loader

---
+++ src/api/loader.ts
@@ -0,0 +1,19 @@
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+const getModules = () : string[] =>
+  readdirSync(join(__dirname, '/controllers/')).filter(file =>
+    (file.endsWith('.js')))
+
+const getRouters = (modulos : string[]) : any[]  =>
+  modulos.map((modulo) => {
+    return require(`./controllers/${modulo}`).default
+  })
+  
+  
+function bootstrap () {
+  const modules : string[] = getModules()
+  return getRouters(modules || [''])
+}
+
+export default bootstrap
