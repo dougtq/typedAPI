35490294e3719b35315ef8cb6a9dc7608dc310ea
Douglas Alves <douglas.expalves@gmail.com>
2018-01-05 03:11:14 -0200

    update mongo.promise and connection promise rejection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,13 @@
 import Mongo = require('mongoose')
 import bluebird = require('bluebird')
 
-bluebird.promisifyAll(Mongo)
+Mongo.Promise = global.Promise
 
 const Connection = Mongo.connect(process.env.mongo_uri || 'mongodb://localhost/typed', {
   useMongoClient: true
 })
 
-export default Connection.then(db => db)
+export default Connection.then(db => db).catch((err) => { 
+  console.error(err) 
+  process.exit(1)
+})
