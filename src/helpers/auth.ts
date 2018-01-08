315e4b91469f44573d51f8422b213a7e8f4c0685
Douglas Alves <douglas.expalves@gmail.com>
2018-01-07 23:17:54 -0200

    update all Token class methods to static methods

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -4,7 +4,7 @@ const secretKey : string = process.env.SECRET ||
 (Math.floor((Math.random() * 9999) * Math.random())).toString()
 
 export default class Token {
-  createToken (payload : any) : string | Boolean {
+  static createToken (payload : any) : string | Boolean {
     try {
       const token = sign(payload, secretKey)
       return `Bearer ${token}`
@@ -13,25 +13,25 @@ export default class Token {
     }
   }
 
-  verifyToken (token : string) : Boolean {
+  static verifyToken (token : string) : Boolean {
     try {
-      const valid = verify(this.getRawToken(token), secretKey.toString())
+      const valid = verify(Token.getRawToken(token), secretKey.toString())
       return Boolean(valid)
     } catch (err) {
       return false
     }
   }
 
-  decodeToken (token : string) : Boolean | any  {
+  static decodeToken (token : string) : Boolean | any  {
     try {
-      const payLoad = decode(this.getRawToken(token))
+      const payLoad = decode(Token.getRawToken(token))
       return payLoad
     } catch (err) {
       return false
     }
   }
 
-  getRawToken (token : string) : string {
+  static getRawToken (token : string) : string {
     const rawToken : string = token.split('Bearer').pop().trim()
     return rawToken
   }
